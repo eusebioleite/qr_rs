7ee6f0d34b8b0fecdeb2e03743fcea74231d0eb1
Eusébio Mackenzie Leite <eusebio.leite16@gmail.com>
2026-03-16 22:35:12 -0300

    Did some styling, thinkering around with themes and such

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,10 @@
-use iced::widget::{button, column, text, text_input};
-use iced::Element;
+use iced::alignment::Horizontal;
+use iced::widget::{button, column, container, text_input};
+use iced::{window, Element, Length, Task, Theme};
 use image::Luma;
 use qrcode::QrCode;
 
-pub fn main() -> iced::Result {
-    iced::run(update, view)
-}
-
-fn update(state: &mut State, message: Message) {
-    match message {
-        Message::GenerateQrCode(url) => match generate_qr_code(&url) {
-            Ok(()) => println!("Success!"),
-            Err(_) => println!("Error..."),
-        },
-        Message::ContentChanged(content) => {
-            state.content = content;
-        }
-    }
-}
-
-fn view(state: &State) -> Element<'_, Message> {
-    column![
-        text("Digite a url"),
-        text_input("Type something here...", &state.content).on_input(Message::ContentChanged),
-        button("Criar QR Code").on_press(Message::GenerateQrCode(String::from(&state.content)))
-    ]
-    .spacing(10)
-    .into()
-}
-fn generate_qr_code(url: &String) -> Result<(), Box<dyn std::error::Error>> {
-    // Geração dos bits do QR
-    let code = QrCode::new(url.as_bytes())?;
-
-    // Renderização para imagem (Luma = Tons de cinza/Preto e Branco)
-    let image = code.render::<Luma<u8>>().build();
-
-    // Persistência no storage D: ou C: conforme sua configuração de SSD
-    image.save("qrcode_output.png")?;
-    Ok(())
-}
+// MAIN LOOP
 
 #[derive(Default)]
 struct State {
@@ -47,6 +13,100 @@ struct State {
 
 #[derive(Debug, Clone)]
 enum Message {
-    GenerateQrCode(String),
+    GenerateQrCode,
     ContentChanged(String),
+}
+
+pub fn main() -> iced::Result {
+    let window_settings = window::Settings {
+        size: iced::Size::new(320.0, 180.0), // Tamanho real da janela
+        resizable: false,                    // Opcional: trava o redimensionamento
+        ..Default::default()
+    };
+
+    // Usamos o builder para rodar com as configurações
+    iced::application(
+        || State {
+            content: String::new(),
+        },
+        update,
+        view,
+    )
+    .title(|_state: &State| "QR Code Generator".to_string())
+    .window(window_settings)
+    .theme(|_state: &State| Theme::GruvboxLight)
+    .centered()
+    .run()
+}
+fn update(state: &mut State, message: Message) -> iced::Task<Message> {
+    match message {
+        Message::GenerateQrCode => match generate_qr_code(&state.content) {
+            Ok(()) => println!("Success!"),
+            Err(_) => println!("Error..."),
+        },
+        Message::ContentChanged(content) => {
+            state.content = content;
+        }
+    }
+    Task::none()
+}
+
+fn view(state: &State) -> Element<'_, Message> {
+    let content = column![
+        text_input("Sua URL aqui...", &state.content)
+            .on_input(Message::ContentChanged)
+            .padding(12),
+        button("Gerar")
+            .padding(10)
+            .on_press(Message::GenerateQrCode)
+    ]
+    .spacing(20)
+    .align_x(Horizontal::Center)
+    .max_width(300);
+
+    container(content)
+        .width(Length::Fill)
+        .height(Length::Fill)
+        .center_x(Length::Fill)
+        .center_y(Length::Fill)
+        .padding(20)
+        .into()
+}
+
+// UTILITY FUNCTIONS
+fn generate_qr_code(url: &String) -> Result<(), Box<dyn std::error::Error>> {
+    let file_name: String = clean_filename(url);
+    let code = QrCode::new(url.as_bytes())?;
+    let image = code.render::<Luma<u8>>().build();
+    image.save(format!("{}.png", file_name))?;
+    Ok(())
+}
+
+fn clean_filename(input: &str) -> String {
+    input
+        .chars()
+        .filter(|&c| {
+            !matches!(
+                c,
+                '<' | '>'
+                    | ':'
+                    | '"'
+                    | '/'
+                    | '\\'
+                    | '|'
+                    | '?'
+                    | '*'
+                    | '\''
+                    | ' '
+                    | '!'
+                    | '('
+                    | ')'
+                    | '~'
+                    | '`'
+                    | '@'
+                    | '#'
+                    | '$'
+            ) && !c.is_control()
+        })
+        .collect()
 }
